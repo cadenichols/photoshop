f1c65c9913b02b6f2b32d50d52e890344dd23a8b
cnichols <mellowdious@mail.com>
2015-02-11 15:02:29 -0800

    working paint

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# JS Photoshop
+# JS Paint
 
-Simple color selector / random color generator.
+Simple MS Paint Clone with psychedelia enabled.
+
+First generate some colors, then generate a canvas (several thousand blocks at least).
+Mouse over to color with selected color.
+
+Nonsensical to be sure.
--- client/main.js
+++ client/main.js
@@ -5,6 +5,60 @@ $(document).ready(init);
 function init() {
   $('#add-color').click(addSelectedColor);
   $('#add-random-colors').click(addRandomColor);
+  $('#start').click(clickStart);
+  $('#stop').click(clickStop);
+  $('#add-canvas').click(clickAddCanvas);
+  $('#colors').on('click', '.box', clickBox);
+  $('#canvas').on('mouseenter', '.tiny', enterTiny);
+
+}
+
+var colors = [];
+var timer;
+var index = 0;
+
+function enterTiny() {
+  var color = $('#selected').css('background-color');
+  $(this).css('background-color', color);
+}
+
+function clickAddCanvas() {
+  var size = $('#canvas-size').val() * 1;
+  for (var i = 0; i < size; i++) {
+    var $tiny = $('<div>');
+    $tiny.addClass('tiny');
+    $('#canvas').append($tiny);
+  }
+}
+
+function clickBox() {
+  var color = $(this).css('background-color');
+  $('#selected').css('background-color', color);
+}
+
+function clickStart() {
+  fillColorArray();
+  clickStop();
+  timer = setInterval(looping, 200);
+}
+
+function looping() {
+  if (index === colors.length) {
+    index = 0;
+    fillColorArray();
+  }
+  $('body').css('background-color', colors[index]);
+  index++;
+}
+
+function clickStop() {
+  clearInterval(timer);
+}
+
+function fillColorArray() {
+  colors = $('.box').toArray().map(function(element) {
+    return $(element).css('background-color');
+  });
 }
 
 function addSelectedColor() {
